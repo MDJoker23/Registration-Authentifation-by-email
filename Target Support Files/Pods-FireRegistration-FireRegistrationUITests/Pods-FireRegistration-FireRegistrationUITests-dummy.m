62482eaf851bfdb9449af0539b90c614979509bc
MDJoker23 <adylzhantohtibakiev@gmail.com>
2024-03-26 19:45:06 +0500

    Initial Commit

---
+++ Target Support Files/Pods-FireRegistration-FireRegistrationUITests/Pods-FireRegistration-FireRegistrationUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FireRegistration_FireRegistrationUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FireRegistration_FireRegistrationUITests
+@end
---
+++ Target Support Files/Pods-FireRegistration/Pods-FireRegistration-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FireRegistration : NSObject
+@end
+@implementation PodsDummy_Pods_FireRegistration
+@end
